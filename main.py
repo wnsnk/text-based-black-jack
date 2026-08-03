8e4dcf32eaac1693922d5c04670b6bf81ff7f6b8
wnsnk <236621219+wnsnk@users.noreply.github.com>
2026-08-04 00:54:25 +0200

    add start game

--- main.py
+++ main.py
@@ -14,4 +14,6 @@ for num in range(PLAYERS):
     player = Player(num + 1)
     player_list.append(player)
 
+# dealer.take_bets(player_list)
 dealer.deal_starting_cards(player_list)
+dealer.start_game(player_list)
